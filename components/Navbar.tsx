27685870881beefb1d4d846c40f483efa8e24716
Roy@Souharda <b123134@iiit-bh.ac.in>
2025-11-04 22:00:43 +0530

    implemented posthog

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,17 +4,17 @@ import Image from "next/image";
 const Navbar = () => {
     return (
         <header>
-            <nav>
-                <Link href={'/'} className={'logo'}>
-                    <Image src={'/icons/logo.png'} alt={'logo'} width={24} height={24}/>
-                    <p>DevZig</p>
-                </Link>
-                <ul>
-                    <Link href={'/'}>Home</Link>
-                    <Link href={'/'}>Events</Link>
-                    <Link href={'/'}>Create Event</Link>
-                </ul>
-            </nav>
+        <nav>
+            <Link href={'/'} className={'logo'}>
+                <Image src={'/icons/logo.png'} alt={'logo'} width={24} height={24}/>
+                <p>DevZig</p>
+            </Link>
+            <ul>
+                <Link href={'/'}>Home</Link>
+                <Link href={'/'}>Events</Link>
+                <Link href={'/'}>Create Event</Link>
+            </ul>
+        </nav>
         </header>
     )
 }
