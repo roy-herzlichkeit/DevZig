a8f8a8f49adc75502ae42d24f3979b7bf0920e30
Roy@Souharda <b123134@iiit-bh.ac.in>
2025-11-05 23:51:53 +0530

    Implemented Event Page, Booking side remains

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,21 +1,79 @@
 import React from 'react'
 import {notFound} from "next/navigation";
+import Image from "next/image";
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const EventDetailItem = ({icon, alt, label}: {icon: string; alt: string; label: string}) => (
+    <div className={`flex-row-gap-2 items-center`}>
+        <Image src={icon} alt={alt} width={17} height={17}/>
+        <p>{label}</p>
+    </div>
+);
+
+const EventAgenda = ({agendaItems}: {agendaItems: string[]}) => (
+    <div className="agenda">
+        <h2>Agenda</h2>
+        <ul>
+            {agendaItems.map((item) => (
+                <li key={item}>{item}</li>
+            ))}
+        </ul>
+    </div>
+);
+
+const EventTags = ({tags}: {tags: string[]}) => (
+    <div className="flex flex-row gap-1.5 flex-wrap">
+        {tags.map((tag) => (
+            <div className="pill" key={tag}>
+                {tag}
+            </div>
+        ))}
+    </div>
+)
 
 const EventDetailsPage = async ({ params } : { params : Promise<{slug : string}> }) => {
     const { slug } = await params;
     const req = await fetch(`${BASE_URL}/api/events/${slug}`);
-    const { event } = await req.json();
-    if (!event) {
+    const { event: { description, image, agenda, overview, date, time, location, mode, audience, tags, organizer } } = await req.json();
+    if (!time) {
         return notFound();
     }
     return (
         <section id="event">
-            <h1>
-                Event Details:
-                <br/>
-                {slug}
-            </h1>
+            <div className="header">
+                <h1>Event Description</h1>
+                <p className={"mt-1"}>{description}</p>
+            </div>
+            <div className="details">
+                <div className="content">
+                    <Image src={image} alt={"Event Banner"} width={800} height={800} className={"banner"} />
+                    <section className="flex-col-gap-2">
+                        <h2>Overview</h2>
+                        <p>{overview}</p>
+                    </section>
+                    <section className="flex-col-gap-2">
+                        <h2>Event Details</h2>
+                        <EventDetailItem icon={"/icons/calendar.svg"} alt={"date"} label={date}/>
+                        <EventDetailItem icon={"/icons/clock.svg"} alt={"time"} label={time}/>
+                        <EventDetailItem icon={"/icons/pin.svg"} alt={"location"} label={location}/>
+                        <EventDetailItem icon={"/icons/mode.svg"} alt={"mode"} label={mode}/>
+                        <EventDetailItem icon={"/icons/audience.svg"} alt={"audience"} label={audience}/>
+                    </section>
+                    <EventAgenda agendaItems={JSON.parse(agenda[0])}/>
+                    <section className="flex-col-gap-2">
+                        <h2>
+                            About the Organiser
+                        </h2>
+                        <p>
+                            {organizer}
+                        </p>
+                    </section>
+                    <EventTags tags={JSON.parse(tags[0])}/>
+                </div>
+                <aside className="booking">
+                    <p className={"text-lg font-semibold"}>Book Event</p>
+                </aside>
+            </div>
         </section>
     )
 }
